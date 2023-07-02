9849552a6c5529a4556cfb2c1a4756dacd95f408
Binatari Akene <akenebina@gmail.com>
2023-07-02 17:05:32 +0100

    add million

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,47 @@
-import React, { useRef } from "react";
+import React, { Suspense, useRef, useMemo } from 'react'
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Box, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Model } from "./components/Scene";
+import Ocean from './components/Ocean';
+
+
+function Box() {
+  const ref = useRef()
+  useFrame((state, delta) => {
+    ref.current.position.y = 10 + Math.sin(state.clock.elapsedTime) * 20
+    ref.current.rotation.x = ref.current.rotation.y = ref.current.rotation.z += delta
+  })
+  return (
+    <mesh ref={ref} scale={20}>
+      <boxGeometry />
+      <meshStandardMaterial />
+    </mesh>
+  )
+}
 
 const Scene = () => {
-  const boxRef = useRef();
-  useFrame((state, delta) => {
-    boxRef.current.rotation.y += 0.02;
-  });
-
   return (
     <>
-      <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
-        <meshNormalMaterial />
-      </Box>
-      <ambientLight />
+      {/* <pointLight intensity={1} color={'blue'}/> */}
+      <Model />
     </>
   );
 };
 
 const App = () => {
   return (
-    <Canvas camera={{ fov: 70, position: [0, 0, 3] }}>
+    <Canvas camera={{ position: [0, 5, 100], fov: 55, near: 1, far: 20000 }}>
+     {/* <ambientLight intensity={0.5} />
+        <spotLight position={[50, 50, -30]} castShadow />
+        <pointLight position={[-10, -10, -10]} color="red" intensity={3} />
+        <pointLight position={[0, -5, 5]} intensity={0.5} />
+        <directionalLight position={[0, -5, 0]} color="red" intensity={2} /> */}
       <OrbitControls />
-      <Scene />
+      <Suspense fallback={null}>
+        <Ocean />
+        <Box />
+      </Suspense>
+      <Environment files={'/wasteland_clouds_puresky_2k.hdr'} background/>
     </Canvas>
   );
 };
